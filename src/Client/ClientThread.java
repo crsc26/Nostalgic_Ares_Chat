8ecb06ac49b78bec7da107d20bb2c4ed5911d24c
Crsc <crsc_26@hotmail.com>
2018-11-02 23:33:07 -0600

    minor changes

--- src/Client/ClientThread.java
+++ src/Client/ClientThread.java
@@ -4,16 +4,18 @@ import Server.Server;
 
 import java.net.Socket;
 
-public class ClientThread extends Server implements Runnable {
-
+public class ClientThread extends Server implements Runnable
+{
     Socket socket;
 
-    public ClientThread(Socket socket){
+    public ClientThread(Socket socket)
+    {
         this.socket = socket;
     }
 
     @Override
-    public void run() {
+    public void run()
+    {
 
     }
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -7,28 +7,40 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
-public class Server {
+public class Server
+{
     static ServerSocket serverSocket;
     static ArrayList<ClientThread> clients;
 
-    public static void acceptClients() {
+    public static void acceptClients()
+    {
         clients = new ArrayList<ClientThread>();
-        while(true){
-            try {
+        while(true)
+        {
+            try
+            {
                 Socket socket = serverSocket.accept();
-            } catch (IOException e) {
+            }
+            catch(IOException e)
+            {
                 e.printStackTrace();
             }
         }
     }
 
-    public static void main(String[] args) {
-        int portNo = 4444;
-        try {
+    // Setting up of serverSocket
+    public static void main(String[] args)
+    {
+        int portNo = 6666;
+
+        try
+        {
             serverSocket = null;
             ServerSocket serverSocket = new ServerSocket(portNo);
             acceptClients();
-        } catch (IOException e) {
+        }
+        catch(IOException e)
+        {
             e.printStackTrace();
             System.out.println("Could not listen to port: " + portNo);
             System.exit(1);
